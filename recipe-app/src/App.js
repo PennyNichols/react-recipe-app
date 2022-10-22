ca9829e53e0d3ac604335be83aec6a79fd15acf3
pnichols789 <pnichols789@gmail.com>
2022-10-22 17:36:15 -0500

    going smooth :)

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,9 +1,16 @@
+import {Routes, Route} from 'react-router-dom'
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/home' element={<h1>Home</h1>}/>
+        <Route path='/about' element={<h1>About</h1>}/>
+        <Route path='/github' element={<h1>GitHub</h1>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ recipe-app/src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Button, Form, Input} from 'reactstrap'
+import {SiCodechef} from 'react-icons/si'
+import {useNavigate} from 'react-router-dom'
+
+const Login = () => {
+    const navigate= useNavigate()
+    const handleSubmit = e =>{
+        e.preventDefault();
+        navigate('/home')
+    }
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <SiCodechef />
+        <h2>Penny's Recipes</h2>
+        <Input type="text" placeholder="Username" required />
+        <Input type="text" placeholder="Password" required />
+        <Button type='submit' >Log In</Button>
+      </Form>
+    </div>
+  )
+}
+
+export default Login
