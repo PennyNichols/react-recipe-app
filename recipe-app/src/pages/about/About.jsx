887bf931a235da4ef0fae0471407c2db559887f2
pnichols789 <pnichols789@gmail.com>
2022-10-25 18:30:01 -0500

    -

--- recipe-app/src/pages/about/About.jsx
+++ recipe-app/src/pages/about/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
         <div className="home-content-wrapper d-flex justify-content-around">
           <img className='about-brand' src={logo} alt='Penny Nichols Logo'/>
           <div className='about-info'>
-              <p className='display-6 text-center'>About Software Developer</p> 
-              <p className='display-5 text-center fw-bold'>Penny Nichols</p>
+              <p className='title display-6 text-center mt-5'>About Software Developer</p> 
+              <p className='title display-5 text-center fw-bold'>Penny Nichols</p>
               <div className='border rounded text-end p-5'>
                 
               </div>
